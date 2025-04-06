0150f945fcd581e97369e4c3d243bd09bebf90e5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-05 22:09:00 -0300

    chore: command handler interface

---
+++ CleanArchEnablers.Framework.Cqrs/Handlers/ICommandHandler.cs
@@ -0,0 +1,6 @@
+namespace CleanArchEnablers.Framework.Cqrs.Handlers;
+
+public interface ICommandHandler<in TCommand, TCommandResult>
+{
+    Task<TCommandResult> Handle(TCommand query, CancellationToken cancellationToken);
+}
