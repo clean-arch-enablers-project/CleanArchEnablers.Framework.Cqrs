ca8e6501fa3f1b08478e65b8638d7cacd8982bd0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-05 22:11:13 -0300

    refactor: command parameter

--- CleanArchEnablers.Framework.Cqrs/Handlers/ICommandHandler.cs
+++ CleanArchEnablers.Framework.Cqrs/Handlers/ICommandHandler.cs
@@ -2,5 +2,5 @@ namespace CleanArchEnablers.Framework.Cqrs.Handlers;
 
 public interface ICommandHandler<in TCommand, TCommandResult>
 {
-    Task<TCommandResult> Handle(TCommand query, CancellationToken cancellationToken);
+    Task<TCommandResult> Handle(TCommand command, CancellationToken cancellationToken);
 }
