37116b2bde2be94ab935c86ffa12605112d0b137
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-05 23:00:23 -0300

    chore: user entity

---
+++ CleanArchEnablers.Framework.Cqrs.Example/Infrastructure/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchEnablers.Framework.Cqrs.Example.Infrastructure.Entities;
+
+[Table("Users")]
+public class UserEntity
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool IsBlocked { get; set; }
+}
